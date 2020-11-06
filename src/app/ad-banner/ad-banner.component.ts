1e4f02e1934bfc091c277398c8df3ec470b126bd
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-06 16:15:59 -0500

    created ad-item and ad.component, completed ad-banner

--- src/app/ad-banner/ad-banner.component.ts
+++ src/app/ad-banner/ad-banner.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AdItem} from "../ad-item";
+import {viewClassName} from "@angular/compiler";
+import {createViewChildren} from "@angular/compiler/src/core";
+import {AdDirective} from "../ad.directive";
+import {AdComponent} from "./ad.component";
 
 @Component({
   selector: 'app-ad-banner',
   templateUrl: './ad-banner.component.html',
   styleUrls: ['./ad-banner.component.css']
 })
-export class AdBannerComponent implements OnInit {
+export class AdBannerComponent implements OnInit, OnDestroy {
+  // passing an array of components to AdBannerComponent allows for a dynamic list of ads without
+  // static elements in the template
+  @Input() ads: AdItem[];
 
-  constructor() { }
+  currentAdIndex = -1;
 
-  ngOnInit(): void {
+  @ViewChild(AdDirective, {static: true}) adHost: AdDirective;
+  interval: any;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  ngOnInit(){
+    this.loadComponent();
+    this.getAds();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  loadComponent(){
+    this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
+    const adItem = this.ads[this.currentAdIndex];
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+
+    const viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent<AdComponent>(componentFactory);
+    componentRef.instance.data = adItem.data;
+  }
+
+  getAds(){
+    this.interval = setInterval(() => {
+      this.loadComponent();
+    }, 3000);
   }
 
 }
